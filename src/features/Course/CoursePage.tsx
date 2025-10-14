081cc509dbc5f42491adedd2f3f43bacc16eba62
Dave <davidglogowski2003@gmail.com>
2025-10-14 02:12:45 +0200

    feat(path): Add button and path

--- src/features/Course/CoursePage.tsx
+++ src/features/Course/CoursePage.tsx
@@ -1,18 +1,25 @@
 import { CommonHeader } from "../../components/Header/CommonHeader";
+import { PathButton } from "./PathButton";
+import { PathRow } from "./PathRow";
 
-export function CoursePage () {
+export function CoursePage() {
+  const mockLessons = [1, 2, 3, 4, 5, 6, 7, 8];
 
-    return (
-
+  return (
     <div className="grid grid-cols-12 grid-rows-[auto_1fr_auto] min-h-screen">
-        <CommonHeader>
-            <div></div>
-        </CommonHeader>
+      <CommonHeader>
+        <div></div>
+      </CommonHeader>
 
-
+      <div className="col-span-full flex justify-center p-6">
+        <div className="w-1/2 flex flex-col gap-8">
+          {mockLessons.map((lesson, index) => (
+            <PathRow index={index}>
+              <PathButton />
+            </PathRow>
+          ))}
+        </div>
+      </div>
     </div>
-
-    )
-
-
+  );
 }
---
+++ src/features/Course/PathButton.tsx
@@ -0,0 +1,7 @@
+export function PathButton() {
+  return (
+    <button className="w-22 h-22 active:translate-y-[8px] active:shadow-none shadow-buttonShadow rounded-3xl bg-ludoGrayLight">
+        
+    </button>
+  );
+}
