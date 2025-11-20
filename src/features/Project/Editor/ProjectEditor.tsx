e8e720a2c155b3c13150d19c816fcdb8e5c2e083
Dave <davidglogowski2003@gmail.com>
2025-11-20 16:46:49 +0100

    refactor(monaco): Extract monaco theme into hook

--- src/features/Project/Editor/ProjectEditor.tsx
+++ src/features/Project/Editor/ProjectEditor.tsx
@@ -1,3 +1,4 @@
+import { useMonacoTheme } from "@/Hooks/UI/useMonacoTheme";
 import Editor, { type BeforeMount, type OnMount } from "@monaco-editor/react";
 type ProjectEditorProps = {
   path: string;
@@ -12,21 +13,7 @@ export function ProjectEditor({
   value,
   onChange,
 }: ProjectEditorProps) {
-  const beforeMount: BeforeMount = (monaco) => {
-    monaco.editor.defineTheme("custom-theme", {
-      base: "vs-dark",
-      inherit: true,
-      rules: [],
-      colors: {
-        "editor.background": "#22273E",
-        "editorGutter.background": "#22273E",
-      },
-    });
-  };
-
-  const onMount: OnMount = (editor, monaco) => {
-    monaco.editor.setTheme("custom-theme"); // ensure applied
-  };
+  const { beforeMount, editorOptions } = useMonacoTheme();
 
   return (
     <Editor
@@ -36,18 +23,8 @@ export function ProjectEditor({
       value={value}
       onChange={(v) => onChange(v ?? "")}
       beforeMount={beforeMount}
-      onMount={onMount}
       language={language}
-      options={{
-        minimap: { enabled: false },
-        fontSize: 16,
-        padding: { bottom: 16 },
-        scrollBeyondLastLine: false,
-        cursorSurroundingLines: 8,
-        cursorSurroundingLinesStyle: "all",
-        renderLineHighlight: "none",
-        renderLineHighlightOnlyWhenFocus: false,
-      }}
+      options={editorOptions}
     />
   );
 }
