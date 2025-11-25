d560e1418a4f60e6738a0f96419ab62ba6e0cfde
Dave <davidglogowski2003@gmail.com>
2025-11-25 15:57:59 +0100

    refactor(module page): Move selection bar under header in mobile - This way it stays in the 'page'

--- src/features/Module/ModuleSelectionBar.tsx
+++ src/features/Module/ModuleSelectionBar.tsx
@@ -1,5 +1,10 @@
 import { HollowSlotButton } from "@/components/Atoms/Button/HollowSlotButton";
+import { PythonIcon } from "@/components/Atoms/Icons/CustomIcon";
+import { HollowSlotButtonGroup } from "@/components/Molecules/Group/HollowSlotButtonGroup";
 import { cn } from "@/lib/utils";
+import { ludoNavigation } from "@/routes/ludoNavigation";
+import { router } from "@/routes/router";
+import { ArrowsRightLeftIcon } from "@heroicons/react/24/solid";
 
 type ModuleSelectionBarProps = { className?: string };
 
@@ -7,12 +12,17 @@ export function ModuleSelectionBar({ className }: ModuleSelectionBarProps) {
   return (
     <div
       className={cn(
-        "h-10 flex justify-center text-white w-full bg-ludoGrayLight",
+        "h-12 flex items-center justify-center text-white w-full bg-ludoGrayLight",
         className
       )}
     >
-      <HollowSlotButton className="h-7">
+      <HollowSlotButton
+        className="w-3/4 h-8 gap-4"
+        onClick={() => router.navigate(ludoNavigation.courseRoot())}
+      >
+        <PythonIcon className="h-6" />
         <p>Module 1</p>
+        <ArrowsRightLeftIcon className="h-6 text-white" />
       </HollowSlotButton>
     </div>
   );
