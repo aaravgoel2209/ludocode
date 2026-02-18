f3293d7881473c265673b718763a51348f72039b
Dave <davidglogowski2003@gmail.com>
2026-02-19 00:57:37 +0100

    feat(subscriptions): Add checkout hook & sync + confirmation page (#250)

--- apps/web/src/features/Subscription/Comparison/SubscriptionComparisonPage.tsx
+++ apps/web/src/features/Subscription/Comparison/SubscriptionComparisonPage.tsx
@@ -11,22 +11,25 @@ export function SubscriptionComparisonPage() {
     qo.courseProgress(currentCourseId),
   );
 
-  const {data: plans} = useSuspenseQuery(qo.plans())
+  const { data: plans } = useSuspenseQuery(qo.plans());
+  const { data: currentPlan } = useSuspenseQuery(qo.subscription());
+
+  const navigateToCurrentModule = () => {
+    router.navigate(
+      ludoNavigation.hub.module.toModule(
+        currentCourseProgress.courseId,
+        currentCourseProgress.moduleId,
+      ),
+    );
+  };
 
   return (
     <div className="w-full h-full grid grid-cols-12">
       <div className="col-span-1 h-full" />
-      <div className="flex flex-col items-center col-span-10 relative gap-8">
-        <div className="absolute hover:cursor-pointer top-0 right-0">
+      <div className="flex flex-col items-center min-h-0 overflow-y-auto pb-6 col-span-10 relative gap-8">
+        <div className="absolute hover:cursor-pointer top-1 right-0">
           <XIcon
-            onClick={() =>
-              router.navigate(
-                ludoNavigation.hub.module.toModule(
-                  currentCourseProgress.courseId,
-                  currentCourseProgress.moduleId,
-                ),
-              )
-            }
+            onClick={() => navigateToCurrentModule()}
             className="text-ludoAltText"
           />
         </div>
@@ -38,9 +41,13 @@ export function SubscriptionComparisonPage() {
             Level up your coding journey with the plan that fits your goals.
           </p>
         </div>
-        <div className="flex flex-col lg:flex-row w-full justify-center gap-5 items-stretch max-w-4xl">
+        <div className="flex flex-col lg:flex-row w-full justify-center gap-6 lg:gap-5 items-stretch max-w-4xl">
           {plans.map((plan) => (
-            <SubscriptionOverviewCard plan={plan} />
+            <SubscriptionOverviewCard
+              navToCurrent={navigateToCurrentModule}
+              current={currentPlan.planCode}
+              plan={plan}
+            />
           ))}
         </div>
       </div>
